43706231375cb9f5fe2a6651afa00433ba7d1263
OrlandoTele96 <orlandoglezmusic@gmail.com>
2019-04-18 14:47:51 -0500

    It can read and open an image now

--- Mapa_CU.py
+++ Mapa_CU.py
@@ -1,3 +1,15 @@
 from tkinter import *
+from tkinter import Label
 
+#window init
 window=Tk()
+#window settings
+window.title("CU waze")
+window.resizable(True,False)
+window.geometry("1020x1020")
+window.configure(background = 'black')
+#Getting image
+mapa=PhotoImage(file="mapacu.gif")
+widget = Label(window, image=mapa).pack()
+#Closing
+window.mainloop()
